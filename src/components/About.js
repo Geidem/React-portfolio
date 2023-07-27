2d5b5245ef62959a9072451c8690374ca8506e72
Unbroken <gibby.eidem@gmail.com>
2023-07-27 01:45:57 -0600

    Attempt to setup navbar props

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About() {
+    return (
+    <div class = "flex-row">
+        <p className= "medium content"> Business owner and aspiring something-of-a Full Stack Developer </p>
+        <hr/>
+        <p className= "intro content"> 
+        My name is Geofferey "Gibby" Eidem, I'm a 29 years old and I am looking to change career paths and explore the world of web development and coding. I have owned and operated my own business for the past 4 years, 
+        yet I've always had a passion for technology and learning. Enrolling in the University of Utah's Coding Bootcamp has been a great experience so far and I am excited to see where this newfound knowledge will take me.
+        </p> 
+    </div>
+    );
+}
+
+export default About;
---
+++ src/components/Navigation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Navigation(props) {
+    const tabs = ['About', 'Portfolio', 'Contact', 'Resume'];
+    return (
+        <div className="navbar is-centered">
+            <ul className="nav nav-tabs"> 
+                {tabs.map(tab => (
+                    <li className={props.currentPage === tab ? 'nav-item active' : 'nav-item'} key={tab}>
+                        <a
+                        href={'#' + tab.toLowerCase()}
+                        onClick={() => props.handlePageChange(tab)}
+                        className={props.currentPage === tab ? 'nav-link active' : 'nav-link'}>
+                            {tab}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Navigation;
